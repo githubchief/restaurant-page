1a1dfc5b4a5641edac4f329e7535279e78f81b05
githubchief <ranganathanshams@gmail.com>
2023-01-09 15:26:51 +0530

    add footer module and import to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 
 import { header } from './header';
+import { footer } from './footer';
 import './style.css';
 
 //content div to add contents for the page
 let contentDiv = document.getElementById("content");
 
 header(contentDiv);
+
+
+footer(contentDiv);
