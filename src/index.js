5aa0659c71f281525a60666d183c6007f63fb804
githubchief <ranganathanshams@gmail.com>
2023-01-09 19:35:46 +0530

    adds event listener and corresponding module to btns

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import { header } from './header';
 import { footer } from './footer';
 import './style.css';
 
+
 //content div to add contents for the page
 let contentDiv = document.getElementById("content");
 
 header(contentDiv);
 
+let buttons=document.querySelectorAll("button");
+
+buttons.forEach( (element )=> {
+
+    if(element.textContent == "HOME"){
+        home(contentDiv);
+    }
+    if(element.textContent == "MENU"){
+        menu(contentDiv);
+    }
+    if(element.textContent == "CONTACT"){
+        contact(contentDiv);
+    }
+
+});
 
 footer(contentDiv);
