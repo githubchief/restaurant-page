d673d47a9d2ece3c376621edcdf3d12b3acb87c2
githubchief <ranganathanshams@gmail.com>
2023-01-09 22:46:39 +0530

    add container for home, menu and contact pages

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
 
 import { header } from './header';
-import { footer } from './footer';
+import { home } from './home';
+import { menu } from './menu';
+import { contact } from './contact';
+import { footer } from './footer'; 
+
 import './style.css';
 
 
 //content div to add contents for the page
 let contentDiv = document.getElementById("content");
-
+let container = document.createElement("div");
+container.className = "container";
 header(contentDiv);
 
 let buttons=document.querySelectorAll("button");
+home(contentDiv, container);
 
 buttons.forEach( (element )=> {
-
-    if(element.textContent == "HOME"){
-        home(contentDiv);
-    }
-    if(element.textContent == "MENU"){
-        menu(contentDiv);
-    }
-    if(element.textContent == "CONTACT"){
-        contact(contentDiv);
-    }
+    
+    if(element.textContent == " HOME "){
+        element.addEventListener("click", () => {
+            home(contentDiv, container);
+            //footer(contentDiv);
+    });}
+    if(element.textContent == " MENU "){
+        element.addEventListener("click", () => {
+            menu(contentDiv, container);
+            //footer(contentDiv);
+    });}
+    if(element.textContent == " CONTACT "){
+        element.addEventListener("click", () => {
+            contact(contentDiv, container);
+            //footer(contentDiv);
+    });}
 
 });
 
